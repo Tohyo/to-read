39200c09580879cfa69631abb68d06c9e135baea
Tohyo <k.baujard@orange.fr>
2023-06-18 15:49:24 +0200

    refresh list after adding article

--- assets/controllers/paste_controller.js
+++ assets/controllers/paste_controller.js
@@ -7,13 +7,14 @@ import { Controller } from '@hotwired/stimulus';
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
     connect() {
-        document.addEventListener("paste", (event) => {
-            fetch('/articles', {
+        document.addEventListener("paste", async (event) => {
+            const response = await fetch('/articles', {
                 method: 'POST',
                 body: JSON.stringify({
                     url: event.clipboardData.getData('text/plain')
                 })
             })
+            this.element.innerHTML = await response.text()
         });
     }
 }
